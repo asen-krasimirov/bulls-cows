9e2671ee12e7fb55e4b1d362f2bcdfe7a3f342b8
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2022-07-15 16:14:29 +0300

    adding git hub link

--- src/components/RulesPage/RulesPage.js
+++ src/components/RulesPage/RulesPage.js
@@ -5,7 +5,10 @@ function RulesPage() {
         <>
             <h2 className="sub-title">Rules</h2>
             <p className="text-holder rules-holder">
-                Bulls and Cows is a 2 player game. One player thinks of a number (the computer in this case), while the other player tries to guess it. The number to be guessed must be a 4 digit number, using only digits from 1 - 9, each digit atmost once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.
+                Bulls and Cows is a 2 player game. One player thinks of a number (the computer in this case), while the other player tries to guess it. The number to be guessed must be a 4 digit number, using only digits from 1 - 9, each digit at most once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.
+            </p>
+            <p className="text-holder rules-holder">
+                Implemented by <a className="git-link" href="https://github.com/asen-krasimirov/bulls-cows">Asen Krasimirov</a>
             </p>
         </>
     );
